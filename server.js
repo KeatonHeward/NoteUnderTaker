c80a6bf89f6d7bf6ee30408428a1baab31903646
Keaton <keat1232@gmail.com>
2022-07-25 12:28:18 -0600

    get routes working

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001
+const notes = require('./db/db.json')
+const path = require('path')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
 app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`))
